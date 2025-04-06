741cdd2d95008f6c6a3f41c13dc6630db76f560c
amruth pai <26.amruth@gmail.com>
2025-04-06 16:17:44 -0500

    'test'

--- src/textSummerizer/utils/common.py
+++ src/textSummerizer/utils/common.py
@@ -15,5 +15,18 @@ def read_yaml(path_to_yaml:Path)-> ConfigBox:
             content=yaml.safe_load(yaml_file)
             logger.info(f'Yaml file {path_to_yaml} loaded successfully')
             return ConfigBox(content)
+    except BoxValueError:
+        raise ValueError('yaml file is empty')
     except Exception as e:
         print(e)
+
+
+@ensure_annotations
+def create_directory(path_to_dir:list, verbose=True):
+    for path in path_to_dir:
+        os.makedirs(path,exist_ok=True)
+        if verbose:
+            logger.info(f'created directory at: {path}')
+
+
+    
